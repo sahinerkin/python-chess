02d6ebce8caaffca8e62c2ee96469b13f18f373e
sahnerkin <sahine17@itu.edu.tr>
2021-09-22 23:47:49 +0300

    Changes for object-oriented approach

---
+++ board.py
@@ -0,0 +1,67 @@
+import pygame
+
+
+class Board:
+
+    # Defining variables for colors and sizes
+    # black_tile_color = (120, 150, 90)
+    # white_tile_color = (240, 240, 210)
+    # margin_size = 50
+    # border_size = 10
+    # bordered_margin_size = margin_size + border_size
+    # tile_size = 75
+    # width = height = 8 * tile_size + 2 * bordered_margin_size
+
+    def __init__(self, black_tile_color, white_tile_color, tile_size, margin_size, border_size):
+        self.black_tile_color = black_tile_color
+        self.white_tile_color = white_tile_color
+        self.tile_size = tile_size
+        self.margin_size = margin_size
+        self.border_size = border_size
+        self.bordered_margin_size = margin_size + border_size
+        self.width = self.height = 8 * tile_size + 2 * self.bordered_margin_size
+
+    # Draw the chess board (return surface to blit on window)
+    def draw(self):
+        board_surface = pygame.Surface((self.width, self.height))
+        # Fill the surface and draw chess board border
+        board_surface.fill(self.white_tile_color)
+        pygame.draw.rect(board_surface, self.black_tile_color, pygame.Rect(self.margin_size, self.margin_size,
+                                                                      8 * self.tile_size + 2 * self.border_size,
+                                                                      8 * self.tile_size + 2 * self.border_size))
+
+        # Draw all the tiles in the fitting places with the corresponding colors
+        for i in range(8):
+            for j in range(8):
+                tile_color = self.white_tile_color if (i + j) % 2 == 0 else self.black_tile_color
+                pygame.draw.rect(board_surface, tile_color,
+                                 pygame.Rect(self.bordered_margin_size + i * self.tile_size,
+                                             self.bordered_margin_size + j * self.tile_size,
+                                             self.tile_size, self.tile_size))
+
+        return board_surface
+
+    # Draw the coordinate indicators outside the board (return the surface)
+    def draw_indicators(self, game_font):
+
+        indicator_surface = pygame.Surface((self.width, self.height), pygame.SRCALPHA)
+        indicator_surface = indicator_surface.convert_alpha()
+
+        for idx in range(8):
+            coord_num = game_font.render(str(idx + 1), True, self.black_tile_color)
+            coord_l_rect = coord_num.get_rect(
+                center=(self.margin_size / 2, self.height - self.bordered_margin_size - (idx + 0.5) * self.tile_size))
+            coord_r_rect = coord_num.get_rect(
+                center=(self.width - self.margin_size / 2, self.height - self.bordered_margin_size - (idx + 0.5) * self.tile_size))
+
+            coord_alpha = game_font.render(chr(97 + idx), True, self.black_tile_color)
+            coord_t_rect = coord_num.get_rect(center=(self.bordered_margin_size + (idx + 0.5) * self.tile_size, self.margin_size / 2))
+            coord_b_rect = coord_num.get_rect(
+                center=(self.bordered_margin_size + (idx + 0.5) * self.tile_size, self.height - self.margin_size / 2))
+
+            indicator_surface.blit(coord_num, coord_l_rect)
+            indicator_surface.blit(coord_num, coord_r_rect)
+            indicator_surface.blit(coord_alpha, coord_t_rect)
+            indicator_surface.blit(coord_alpha, coord_b_rect)
+
+        return indicator_surface
--- main.py
+++ main.py
@@ -1,54 +1,26 @@
 import pygame
+from board import Board
 
-# Defining variables for colors and sizes
-black_tile_color = (120, 150, 90)
-white_tile_color = (240, 240, 210)
-margin_size = 50
-border_size = 10
-bordered_margin_size = margin_size + border_size
-tile_size = 75
-width = height = 8 * tile_size + 2 * bordered_margin_size
+pygame.init()
+board = Board(black_tile_color=(120, 150, 90),
+              white_tile_color=(240, 240, 210),
+              tile_size=75,
+              margin_size=50,
+              border_size=10)
 
+win = pygame.display.set_mode((board.width, board.height))
+pygame.display.set_caption("Python Chess")
 
-# Draw the chess board
-def draw_chess_board():
-    # Fill the surface and draw chess board border
-    surface.fill(white_tile_color)
-    pygame.draw.rect(surface, black_tile_color, pygame.Rect(margin_size, margin_size,
-                                                            8 * tile_size + 2 * border_size,
-                                                            8 * tile_size + 2 * border_size))
-
-    # Draw all the tiles in the fitting places with the corresponding colors
-    for i in range(8):
-        for j in range(8):
-            tile_color = white_tile_color if (i + j) % 2 == 0 else black_tile_color
-            pygame.draw.rect(surface, tile_color,
-                             pygame.Rect(bordered_margin_size + i * tile_size,
-                                         bordered_margin_size + j * tile_size, tile_size, tile_size))
-
-    win.blit(surface, (0, 0))
-
-    # Add coordinate indicators outside the board
-    for idx in range(8):
-        coord_num = game_font.render(str(idx + 1), True, black_tile_color)
-        coord_l_rect = coord_num.get_rect(
-            center=(margin_size / 2, height - bordered_margin_size - (idx + 0.5) * tile_size))
-        coord_r_rect = coord_num.get_rect(
-            center=(width - margin_size / 2, height - bordered_margin_size - (idx + 0.5) * tile_size))
-
-        coord_alph = game_font.render(chr(97 + idx), True, black_tile_color)
-        coord_t_rect = coord_num.get_rect(center=(bordered_margin_size + (idx + 0.5) * tile_size, margin_size / 2))
-        coord_b_rect = coord_num.get_rect(
-            center=(bordered_margin_size + (idx + 0.5) * tile_size, height - margin_size / 2))
-
-        win.blit(coord_num, coord_l_rect)
-        win.blit(coord_num, coord_r_rect)
-        win.blit(coord_alph, coord_t_rect)
-        win.blit(coord_alph, coord_b_rect)
+pygame.font.init()
+game_font = pygame.font.SysFont("Arial", 26)
 
 
 def main():
-    draw_chess_board()
+    board_surface = board.draw()
+    win.blit(board_surface, (0, 0))
+
+    indicator_surface = board.draw_indicators(game_font=game_font)
+    win.blit(indicator_surface, (0, 0))
     pygame.display.update()
 
     run = True
@@ -62,13 +34,5 @@ def main():
             if event.type == pygame.MOUSEBUTTONDOWN:
                 pass
 
-
-pygame.init()
-win = pygame.display.set_mode((width, height))
-surface = pygame.Surface((width, height))
-pygame.display.set_caption("Python Chess")
-
-pygame.font.init()
-game_font = pygame.font.SysFont("Arial", 26)
 
 main()
