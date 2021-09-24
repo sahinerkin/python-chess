66a9a7d0f0ae01bf2c839b59fd6746df0db7c445
sahnerkin <sahine17@itu.edu.tr>
2021-09-24 20:50:03 +0300

    Added Rook and Knight movement

--- board.py
+++ board.py
@@ -123,6 +123,8 @@ class Board:
                         King(PieceColor.Black, self.piece_size, coord_alphanum="d8"),
                         Queen(PieceColor.Black, self.piece_size, coord_alphanum="e8")]
 
+        self.pieces += [Rook(PieceColor.White, self.piece_size, coord_alphanum="d5")]
+
     def get_clicked_place(self, mouse_pos):
         pos_x, pos_y = mouse_pos[0]-self.bordered_margin_size, mouse_pos[1]-self.bordered_margin_size
         pos_x, pos_y = pos_x // self.tile_size, pos_y // self.tile_size
--- piece.py
+++ piece.py
@@ -19,8 +19,6 @@ class Piece:
         pass
 
     def set_position(self, coord_alphanum):
-        alpha = coord_alphanum[0]
-        num = int(coord_alphanum[1])
         self.position = coord_alphanum
         self.position_indexed = Pawn.coord_alphanum_to_index(coord_alphanum)
 
@@ -78,26 +76,46 @@ class Rook(Piece):
         if coord_alphanum:
             self.set_position(coord_alphanum)
 
-    # def moves_available(self, pieces):
-    #     moves = []
-    #     tmp_x, tmp_y = self.position_indexed
-    #     if self.color == PieceColor.White:
-    #         while tmp_y > 0:
-    #             tmp_y -= 1
-    #             tmp_coord_alphanum = Piece.index_to_coord_alphanum(tmp_x, tmp_y)
-    #             if Pawn.piece_in(pieces, tmp_coord_alphanum) is not None:
-    #                 break
-    #             moves.append(tmp_coord_alphanum)
-    #
-    #     else:
-    #         tmp_y += 1
-    #         while tmp_y < 8:
-    #             tmp_coord_alphanum = Piece.index_to_coord_alphanum(tmp_x, tmp_y)
-    #             if Pawn.piece_in(pieces, tmp_coord_alphanum) is not None:
-    #                 break
-    #             moves.append(tmp_coord_alphanum)
-    #
-    #     return moves
+    def moves_available(self, pieces):
+        moves = []
+        tmp_x, tmp_y = self.position_indexed
+
+        while tmp_y > 0:
+            tmp_y -= 1
+            tmp_coord_alphanum = Piece.index_to_coord_alphanum(tmp_x, tmp_y)
+            if Pawn.piece_in(pieces, tmp_coord_alphanum) is not None:
+                break
+            moves.append(tmp_coord_alphanum)
+
+        tmp_x, tmp_y = self.position_indexed
+
+        while tmp_y < 7:
+            tmp_y += 1
+            tmp_coord_alphanum = Piece.index_to_coord_alphanum(tmp_x, tmp_y)
+            if Pawn.piece_in(pieces, tmp_coord_alphanum) is not None:
+                break
+            moves.append(tmp_coord_alphanum)
+
+        tmp_x, tmp_y = self.position_indexed
+
+        while tmp_x > 0:
+            tmp_x -= 1
+            tmp_coord_alphanum = Piece.index_to_coord_alphanum(tmp_x, tmp_y)
+            if Pawn.piece_in(pieces, tmp_coord_alphanum) is not None:
+                break
+            moves.append(tmp_coord_alphanum)
+
+        tmp_x, tmp_y = self.position_indexed
+
+        while tmp_x < 7:
+            tmp_x += 1
+            tmp_coord_alphanum = Piece.index_to_coord_alphanum(tmp_x, tmp_y)
+            if Pawn.piece_in(pieces, tmp_coord_alphanum) is not None:
+                break
+            moves.append(tmp_coord_alphanum)
+
+        return moves
+
 
 class Knight(Piece):
     def __init__(self, color, size, coord_alphanum=None):
@@ -106,6 +124,28 @@ class Knight(Piece):
         self.img = pygame.transform.scale(self.img, (size, size)).convert_alpha()
         if coord_alphanum:
             self.set_position(coord_alphanum)
+
+    def moves_available(self, pieces):
+        moves = []
+
+        tmp_x, tmp_y = self.position_indexed
+
+        possible_moves = [(tmp_x+1, tmp_y-2), (tmp_x+2, tmp_y-1),
+                          (tmp_x+2, tmp_y+1), (tmp_x+1, tmp_y+2),
+                          (tmp_x-1, tmp_y+2), (tmp_x-2, tmp_y+1),
+                          (tmp_x-2, tmp_y-1), (tmp_x-1, tmp_y-2)]
+
+        for move in possible_moves:
+            if move[0] < 0 or move[0] > 7 or move[1] < 0 or move[1] > 7:
+                continue
+
+            tmp_coord_alphanum = Piece.index_to_coord_alphanum(move[0], move[1])
+            if Piece.piece_in(pieces, tmp_coord_alphanum) is not None:
+                continue
+
+            moves.append(tmp_coord_alphanum)
+
+        return moves
 
 
 class Bishop(Piece):
@@ -133,4 +173,3 @@ class Queen(Piece):
         self.img = pygame.transform.scale(self.img, (size, size)).convert_alpha()
         if coord_alphanum:
             self.set_position(coord_alphanum)
-
